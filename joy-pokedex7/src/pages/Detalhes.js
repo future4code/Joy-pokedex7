a443820288b186b2bf93e6e9ba7fc1a236ca230d
FredNeves95 <fredbneves95@gmail.com>
2022-02-05 16:13:33 -0300

    navegação para pagina de detalhes

--- joy-pokedex7/src/pages/Detalhes.js
+++ joy-pokedex7/src/pages/Detalhes.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Detalhes = () => {
     const history = useHistory()
+    const params = useParams()
+    const [pokemon, setPokemon] = useState()
+    console.log(pokemon)
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`)
+            .then((res) => {
+                setPokemon(res.data)
+            })
+            .catch((err) => {
+                alert(err)
+            })
+    }, [])
+
     return <div>
         <p>Detalhes</p>
     </div>;
--- joy-pokedex7/src/routes/Router.js
+++ joy-pokedex7/src/routes/Router.js
@@ -12,7 +12,7 @@ const Router = () => {
                 <Home />
             </Route>
 
-            <Route exact path={"/detalhes"}>
+            <Route exact path={"/detalhes/:pokemon"}>
                 <Detalhes />
             </Route>
 
