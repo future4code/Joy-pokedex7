3edc1ba3747d7639b1455cde4333674eb238c28a
FredNeves95 <fredbneves95@gmail.com>
2022-02-07 22:04:53 -0300

    paginação

--- joy-pokedex7/src/App.js
+++ joy-pokedex7/src/App.js
@@ -6,24 +6,41 @@ import { useState, useEffect, useContext } from 'react';
 
 function App() {
 
-  const [pokemon , setPokemon] = useState([])
-  
-  useEffect(()=>{
+  const [pokemon, setPokemon] = useState([])
+  const [page, setPage] = useState(1)
+  const [offset, setOffset] = useState()
+  useEffect(() => {
     getPokemon()
-  }, [])
+  }, [offset])
 
-  const getPokemon = ()=>{
-    axios.get("https://pokeapi.co/api/v2/pokemon")
-    .then((response)=>{
-      setPokemon(response.data.results)
-    })
-    .catch((err)=>{
-      alert(err)
-    })
+  const getPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`)
+      .then((response) => {
+        setPokemon(response.data.results)
+        console.log(response.data);
+      })
+      .catch((err) => {
+        alert(err)
+      })
+  }
+
+  const values = {
+    pokemon,
+    offset,
+    page
+  }
+
+  const setters = {
+    setOffset,
+    setPage,
+  }
+
+  const getters = {
+    getPokemon
   }
 
   return (
-    <PokemonContext.Provider value={pokemon}>
+    <PokemonContext.Provider value={{ values, setters, getters }}>
       <Router />
     </PokemonContext.Provider>
   );
--- joy-pokedex7/src/pages/Home.js
+++ joy-pokedex7/src/pages/Home.js
@@ -2,20 +2,42 @@ import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import PokemonContext from '../contexts/context';
 import PokemonCard from '../components/PokemonCard';
+import { Pagination, Typography } from '@mui/material';
 
 const Home = () => {
-    const pokemon = useContext(PokemonContext)
+    const { values } = useContext(PokemonContext)
+    const { setters } = useContext(PokemonContext)
+    const { getters } = useContext(PokemonContext)
+
+    const pokemon = values.pokemon
+
+    const offset = values.offset
+    const setOffset = setters.setOffset
+
+    const page = values.page
+    const setPage = setters.setPage
+
+    const getPokemon = getters.getPokemon
+
     const history = useHistory()
 
-    const cardsPoke = pokemon.map((poke)=>{
-        return(
-            <PokemonCard key={poke.name} url = {poke.url}/>
+    const cardsPoke = pokemon.map((poke) => {
+        return (
+            <PokemonCard key={poke.name} url={poke.url} />
         )
     })
+
+    const handleChange = (event, value) => {
+        setPage(value)
+        setOffset((value - 1) * 20)
+        getPokemon()
+    }
 
     return <div>
         <p>Home</p>
         {cardsPoke}
+        <Typography>Página: {page}</Typography>
+        <Pagination count={56} variant="outlined" color="primary" shape='rounded' page={page} onChange={handleChange} />
     </div>;
 };
 
