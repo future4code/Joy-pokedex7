6942665f915b962046baf2dbc615155de2c323cc
FredNeves95 <fredbneves95@gmail.com>
2022-02-10 22:01:13 -0300

    bug de renderização e botões de navegação

--- joy-pokedex7/src/pages/Detalhes.js
+++ joy-pokedex7/src/pages/Detalhes.js
@@ -8,7 +8,6 @@ const Detalhes = () => {
     const history = useHistory()
     const params = useParams()
     const [pokemon, setPokemon] = useState()
-    console.log(pokemon)
 
     useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`)
@@ -20,35 +19,51 @@ const Detalhes = () => {
             })
     }, [])
 
+    const goToHome = () => {
+        history.push(`/`)
+    }
+
+    const goToPokedex = () => {
+        history.push(`/pokedex`)
+    }
+
     if (pokemon) {
+
+        const types = pokemon.types.map((item) => {
+            return (<p>
+                {item.type.name}
+            </p>)
+        })
+
         return (
             <div>
                 <p>Detalhes</p>
+                <button onClick={goToHome}>Home</button>
+                <button onClick={goToPokedex}>Pokedex</button>
                 <div>
                     <img src={pokemon.sprites.front_default} />
                     <img src={pokemon.sprites.back_default} />
                 </div>
                 <div>
                     <h3>Poderes</h3>
-                    <p><b>hp:</b> {pokemon.stats [0].base_stat}</p>
-                    <p><b>attack:</b> {pokemon.stats [1].base_stat}</p>
-                    <p><b>defense:</b> {pokemon.stats [2].base_stat}</p>
-                    <p><b>special-attack:</b> {pokemon.stats [3].base_stat}</p>
-                    <p><b>special-defense:</b> {pokemon.stats [4].base_stat}</p>
-                    <p><b>speed:</b> {pokemon.stats [5].base_stat}</p>
+                    <p><b>hp:</b> {pokemon.stats[0].base_stat}</p>
+                    <p><b>attack:</b> {pokemon.stats[1].base_stat}</p>
+                    <p><b>defense:</b> {pokemon.stats[2].base_stat}</p>
+                    <p><b>special-attack:</b> {pokemon.stats[3].base_stat}</p>
+                    <p><b>special-defense:</b> {pokemon.stats[4].base_stat}</p>
+                    <p><b>speed:</b> {pokemon.stats[5].base_stat}</p>
                 </div>
                 <div>
                     <div>
-                        <p>{pokemon.types [0].type.name}</p>
-                        <p>{pokemon.types [1].type.name}</p>
+                        {types}
                     </div>
                     <div>
                         <h3>Principais ataques</h3>
-                        <p>{pokemon.moves [0].move.name}</p>
-                        <p>{pokemon.moves [1].move.name}</p>
-                        <p>{pokemon.moves [2].move.name}</p>
-                        <p>{pokemon.moves [3].move.name}</p>
-                        <p>{pokemon.moves [4].move.name}</p>
+                        <p>{pokemon.moves[0].move.name}</p>
+                        <p>{pokemon.moves[1].move.name}</p>
+                        <p>{pokemon.moves[2].move.name}</p>
+                        <p>{pokemon.moves[3].move.name}</p>
+                        <p>{pokemon.moves[4].move.name}</p>
                     </div>
                 </div>
             </div>
